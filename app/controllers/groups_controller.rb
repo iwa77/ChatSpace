9c45d3d40a29be8174e11ecf87b149b86ab3e164
iwa77 <iwa777777@gmail.com>
2017-05-06 20:04:46 +0900

    action-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,44 @@
 class GroupsController < ApplicationController
  
-def new
-end 
+  def new
+    @group = Group.new
 
-def edit
+
+  end 
+
+  def create
+    group = Group.new(post_params)
+    if  group.save
+    redirect_to root_path, notice: 'グループを作成しました'
+    else
+    flash.now[:alert] = 'グループの作成に失敗しました'
+    render :new
+    end
+  end
+    
+  def show
+    @groups = Group.all
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.new(post_params)
+    if  group.save
+    redirect_to group_path, notice: 'グループを編集しました'
+    else
+    flash.now[:alert] = 'グループの編集に失敗しました'
+    render :new
+    end
+  end
+
+private
+
+def post_params
+  params.require(:group).permit(:name, {:user_ids => []})
 end
 
-def create
-end
-
-end
-
-def show
-  @groups = Group.all
-  @group = Group.find(params[:id])
-  @users = Group.find(params[:id]).users
 end
