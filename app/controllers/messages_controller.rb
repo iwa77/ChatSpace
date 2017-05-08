f4db1a765ab2b50c0b23d440f8fbffd5861025c6
iwa77 <iwa777777@gmail.com>
2017-05-08 18:17:20 +0900

    指摘箇所の修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,10 @@ class MessagesController < ApplicationController
   before_action :groups
 
   def index
-    # @groups = current_user.groups
+  	
   end
 
   def new
-    # @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @users = @group.users
   end
