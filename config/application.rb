5e0b20499d9b604987163ba29c8ff4c5c3bf3849
iwa77 <iwa777777@gmail.com>
2017-05-09 18:15:45 +0900

    message-sending-and-display

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.test_framework false
       g.assets     false
